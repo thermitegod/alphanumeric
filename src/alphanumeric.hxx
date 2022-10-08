903b484db66d08d5071358b79ee026581578c01c
thermitegod <thermitethegod@gmail.com>
2022-10-08 04:27:02 -0700

    remove ALPHANUM_LOCALE

--- src/alphanumeric.hxx
+++ src/alphanumeric.hxx
@@ -24,12 +24,7 @@
 #include <string>
 #include <sstream>
 
-// if you want to honour the locale settings for detecting digit
-// characters, you should define ALPHANUM_LOCALE
-#define ALPHANUM_LOCALE
-#ifdef ALPHANUM_LOCALE
 #include <cctype>
-#endif
 
 #ifdef ALPHANUM_ASSERTS
 #include <cassert>
@@ -47,42 +42,16 @@ namespace AlphaNumeric
 {
     namespace
     {
-#ifdef ALPHANUM_LOCALE
-        /**
-         * wrapper function for ::isdigit()
-         */
-        bool
-        alphanum_isdigit(int c)
-        {
-            return isdigit(c);
-        }
-#else
-        /**
-         * this function does not consider the current locale and only
-         * works with ASCII digits.
-         * @return true if c is a digit character
-         */
-        bool
-        alphanum_isdigit(const char c)
-        {
-            return c >= '0' && c <= '9';
-        }
-#endif
-
         /**
          * compare l and r with strcmp() semantics, but using
          * the "Alphanum Algorithm". This function is designed to read
          * through the l and r strings only one time, for
          * maximum performance. It does not allocate memory for
-         * substrings. It can either use the C-library functions isdigit()
-         * and atoi() to honour your locale settings, when recognizing
-         * digit characters when you "#define ALPHANUM_LOCALE=1" or use
-         * it's own digit character handling which only works with ASCII
-         * digit characters, but provides better performance.
+         * substrings.
          *
-         * @param l NULL-terminated C-style string
-         * @param r NULL-terminated C-style string
-         * @return negative if l<r, 0 if l equals r, positive if l>r
+         * @param l C-style string
+         * @param r C-style string
+         * @return negative if left<right, 0 if left==right, positive if left>right.
          */
         int
         alphanum_impl(const char* l, const char* r)
@@ -101,8 +70,8 @@ namespace AlphaNumeric
                     while ((l_char = *l) && (r_char = *r))
                     {
                         // check if this are digit characters
-                        const bool l_digit = alphanum_isdigit(l_char),
-                                   r_digit = alphanum_isdigit(r_char);
+                        const bool l_digit = isdigit(l_char);
+                        const bool r_digit = isdigit(r_char);
                         // if both characters are digits, we continue in NUMBER mode
                         if (l_digit && r_digit)
                         {
@@ -127,7 +96,6 @@ namespace AlphaNumeric
                 }
                 else // mode==NUMBER
                 {
-#ifdef ALPHANUM_LOCALE
                     // get the left number
                     char* end;
                     unsigned long l_int = strtoul(l, &end, 0);
@@ -136,25 +104,6 @@ namespace AlphaNumeric
                     // get the right number
                     unsigned long r_int = strtoul(r, &end, 0);
                     r = end;
-#else
-                    // get the left number
-                    unsigned long l_int = 0;
-                    while (*l && alphanum_isdigit(*l))
-                    {
-                        // TODO: this can overflow
-                        l_int = l_int * 10 + *l - '0';
-                        ++l;
-                    }
-
-                    // get the right number
-                    unsigned long r_int = 0;
-                    while (*r && alphanum_isdigit(*r))
-                    {
-                        // TODO: this can overflow
-                        r_int = r_int * 10 + *r - '0';
-                        ++r;
-                    }
-#endif
 
                     // if the difference is not equal to zero, we have a comparison result
                     const long diff = l_int - r_int;
